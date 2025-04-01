dac54186fd963d3e8ddfa61d4d07b870acba81a1
izzatipmk <nureffaizzati@gmail.com>
2025-04-01 14:11:35 +0100

    migration done

--- models.py
+++ models.py
@@ -1,15 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 db = SQLAlchemy()
-
+ 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)  # Plain text password (intentionally insecure)
+    # password = db.Column(db.String(80), nullable=False)  # Plain text password (intentionally insecure)
+    password_hash = db.Column(db.String(128), nullable=False) 
     is_admin = db.Column(db.Boolean, default=False)
     tasks = db.relationship('Task', backref='assigned_user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        if self.password_hash:
+            return bcrypt.check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
